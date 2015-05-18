f3514c79e4d3a1743decbac779d3f0d2e2beceba
Jeremy Bell <bellfamilynumberone@gmail.com>
2015-05-18 17:00:51 -0400

    Debugging Auto Line Break in Articles

--- content/functions.py
+++ content/functions.py
@@ -4,9 +4,10 @@ def autoLineBreak(uf_string):
         Parameters: uf_string - an unformatted string
         Returns a string dressed up with the <p> html tag
     """
+    print "breaking up the lines!"
     lines = uf_string.split('\n')
     newLines = ""
     for line in lines:
         newLines += "<p>" + line + "</p>"
-
+    print newLines
     return newLines
