0dec2dfd06d1083e5454f892c34c145f936849a2
Jeremy Bell <bellfamilynumberone@gmail.com>
2015-05-19 20:45:01 -0400

    Too Many to Remember

--- content/models.py
+++ content/models.py
@@ -1,21 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class ArticleCategory(models.Model):
+    name = models.CharField(max_length = 30)
+    description = models.CharField(max_length = 250)
+    def __str__(self):
+        return str(self.name)
 class Article(models.Model):
-    TYPE_CHOICES = (
-        ('1', 'science'),
-        ('2', 'arts'),
-        ('3', 'history'),
-        ('4', 'business'),
-        ('5', 'computers'),
-        ('6', 'engineering'),
-        ('7', 'medicine'),
-        ('8', 'everyday')
-    )
-    type = models.MultipleChoiceField(choices=TYPE_CHOICES)
+    type = models.ManyToMany(ArticleCaegory)
     title = models.CharField(max_length = 40)
     keywords = models.TextField()
     background = models.TextField(blank=True)
     description = models.TextField()
     content = models.TextField()
     date = models.DateTimeField(auto_now_add = True, blank=True)
+    def __str__(self):
+        return str(self.title) + "-" + type.
--- content/views.py
+++ content/views.py
@@ -2,27 +2,45 @@ from django.shortcuts import render
 import models
 from django.http import Http404
 from content import functions
+DEFAULTS = [
+    models.ArticleCategory.objects.all(), #For navigation
+]
 def index(request):
     return render(request, 'index.html')
-def article_listings(request, category=''):
-    if category != '':
-        enteredType = '0'
-        for innerTuple in models.Article.TYPE_CHOICES:
-            if category.lower() == innerTuple[1]:
-                enteredType = innerTuple[0]
-                break;
-        if enteredType == '0':
-            raise Http404("Page not found")
-        return  render(request, 'article_listings.html', {'category': category.capitalize(), 'articles': models.Article.objects.filter(type__contains = enteredType)})
-    else:
-        raise Http404("Page not found")
-    return  render(request, 'article_listings.html', {'category': category.capitalize()})
 def all_article_listings(request):
-    return render(request, 'article_listings.html', {'category': 'All Articles', 'articles':models.Article.objects.all()})
+    return render(request, 'article_listings.html', {'category': 'All Articles', 'articles':models.Article.objects.all().sort_by("-date", "title")})
+def article_listings(request, category=''):
+    #If a category was entered...
+    if category != '':
+        articles = '' #initializing to use outside of try-catch
+
+        #Select ArticleCategory corresponding to category entered and select
+        #all articles under this category
+        try:
+            articles = models.ArticleCategory.get(title = category).article_set.all().sort_by("-date", "title")
+        #Category doesn't exist, so 404 Error
+        except:
+            raise Http404("Article Category not found.")
+        #Otherwise, successful!
+        return  render(request, 'article_listings.html', {'category': category.capitalize(), 'articles': articles})
+    #Else return all articles
+    return  all_article_listings(request)
+
+
 def article(request, artID):
-    art = models.Article.objects.get(pk=int(artID))
-    file = 'article.html'
+
+    #Retrieve appropriate article
+    try:
+        art = models.Article.objects.get(pk=int(artID))
+    except:
+        raise Http404("Article not found.")
+    outFile = 'article.html'#Template to use
+
+    #Insert a <p> tag between each line break
     contentStyled = functions.autoLineBreak(art.content)
+
+    #If there is a background url, change the template
     if len(str(art.background)) > 4:
-        file = 'article_with_background.html'
-    return render(request, file, {'article': art, 'content': contentStyled})
+        outFile = 'article_with_background.html'
+
+    return render(request, outFile, {'article': art, 'content': contentStyled})
