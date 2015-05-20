5b55f2f83b202a4baf1cf1d0e80e906b6d3f1a46
Jeremy Bell <bellfamilynumberone@gmail.com>
2015-05-20 16:42:04 -0400

    Not a Bug, but an improvement to handling of urls

--- content/urls.py
+++ content/urls.py
@@ -4,9 +4,9 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'Legacy.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    
+
     url(r'view/(?P<artID>[0-9]+)/$', 'content.views.article'),
-    url(r'(?P<category>[a-z]+)/$', 'content.views.article_listings'),
-    
+    url(r'(?P<category>[A-Za-z]+)/$', 'content.views.article_listings'),
+
     #url(r'$', 'content.views.all_article_listings'),
 )
