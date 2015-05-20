ddcb4ae04b1a793ed152e3793a76802a3b215842
Jeremy Bell <bellfamilynumberone@gmail.com>
2015-05-19 22:40:10 -0400

    Debug Mode on

--- Legacy/settings.py
+++ Legacy/settings.py
@@ -26,7 +26,7 @@ STATICFILES_DIRS = (
 SECRET_KEY = 'eaqd^k^27#uknerairg2@fun43r=he($wa##7larw7ncl11hd_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
